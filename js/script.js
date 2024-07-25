68109ee8f80f83d01b0f86545850c908adfdde03
bagaspra16 <bagaspratamajunianika72@gmail.com>
2024-07-25 10:04:03 +0700

    Second commit: responsive update,new login modal

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-/**
- * WEBSITE: https://themefisher.com
- * TWITTER: https://twitter.com/themefisher
- * FACEBOOK: https://www.facebook.com/themefisher
- * GITHUB: https://github.com/themefisher/
- */
+/*===== CLEAR FORM BEFORE UNLOAD =====*/
+window.onbeforeunload = () => {
+  for(const form of document.getElementsByTagName("form")){
+      form.reset();
+  }
+}
 
 (function ($) {
   'use strict';
@@ -141,3 +141,4 @@
 
 
 })(jQuery);
+
